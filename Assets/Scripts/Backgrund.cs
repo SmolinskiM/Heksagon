854cd73a479192e5d243738b0f9384a388ed51fa
msmolinski <msmolinski.2001@wp.pl>
2021-10-19 18:14:29 +0200

    Finished game

---
+++ Assets/Scripts/Backgrund.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backgrund : MonoBehaviour
+{
+    public Color[] colors;
+    int curent_index;
+    Camera camera;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+
+        SetColor(colors[curent_index]);
+    }
+
+    public void SetColor(Color color)
+    {
+        camera.backgroundColor = color;
+    }
+
+    public void Cycle_color()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player_UI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player_UI : MonoBehaviour
+{
+    public Canvas stop_screen;
+    public Text text;
+    public Score score_curent;
+    public Player player;
+    bool stop_game = false;
+
+    private void Start()
+    {
+        player = GetComponent<Player>();
+        Time.timeScale = 1;
+    }
+
+    private void Update()
+    {
+        text.text = "Score : " + score_curent.score;
+
+        if(Input.GetKeyDown(KeyCode.Escape) && player.is_alive)
+        {
+            if(!stop_game)
+            {
+                Time.timeScale = 0;
+                stop_game = true;
+                stop_screen.gameObject.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                stop_game = false;
+            }
+            stop_screen.gameObject.SetActive(stop_game);
+        }
+    }
+
+    public void Reset()
+    {
+        Time.timeScale = 1;
+        score_curent.score = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void End()
+    {
+        score_curent.score = 0;
+        SceneManager.LoadScene("Menu");
+    }
+}
